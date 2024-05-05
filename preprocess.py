613ac0efdd648bdd3a2c2ef2812ce3eb4a225244
Le <21020322@vnu.edu.vn>
2024-05-05 13:50:55 +0700

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ def preprocess(image):
     :return: 3 channel RGB image
     """
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    thresholded = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY_INV)[1]
+    thresholded = cv2.threshold(gray, 135, 255, cv2.THRESH_BINARY_INV)[1]
     edges = cv2.Canny(thresholded, 50, 150, apertureSize=3)
     RGB_edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 
