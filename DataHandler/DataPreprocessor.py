6605214974694eed376eb8f3f8e88590d09ba84d
Le <21020322@vnu.edu.vn>
2024-05-06 13:37:31 +0700

    Change preprocessor

--- DataHandler/DataPreprocessor.py
+++ DataHandler/DataPreprocessor.py
@@ -10,8 +10,8 @@ class DataPreprocessor:
         :return: 3 channel RGB image
         """
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        thresholded = cv2.threshold(gray, 135, 255, cv2.THRESH_BINARY_INV)[1]
-        edges = cv2.Canny(thresholded, 50, 150, apertureSize=3)
+        #thresholded = cv2.threshold(gray, 135, 255, cv2.THRESH_BINARY_INV)[1]
+        edges = cv2.Canny(gray, 50, 150, apertureSize=3)
         RGB_edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
 
         return RGB_edges
